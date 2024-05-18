325bf3ec34d1d564c8a707c26346f22cb53c0b13
Dav-11 <48259205+Dav-11@users.noreply.github.com>
2024-05-18 10:07:30 +0000

    working version actually usable

--- 03_xdp_dad/xdp_dad.c
+++ 03_xdp_dad/xdp_dad.c
@@ -5,6 +5,11 @@
 #include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <net/if.h>
+#include <linux/if_link.h>
+#include <poll.h>
+
+static __u32 xdp_flags = 0;
 
 static int libbpf_print_fn(enum libbpf_print_level level, const char *format,
                            va_list args) {
@@ -46,15 +51,16 @@ int print_map_data(struct xdp_dad_bpf *skel) {
     printf("\t[IP: %s]\n", ip_str);
 
     // Lookup the map entry by the current key
-    int res = bpf_map__lookup_elem(skel->maps.xdp_stats_map, &next_key, sizeof(__u32), &entry, sizeof(struct arp_entry), BPF_ANY);
+    int res =
+        bpf_map__lookup_elem(skel->maps.xdp_stats_map, &next_key, sizeof(__u32),
+                             &entry, sizeof(struct arp_entry), BPF_ANY);
     if (res == 0) {
 
       // Process the entry (for example, print MAC addresses)
       for (int i = 0; i < entry.size; i++) {
-        printf("\t\t\t[%d] [MAC: %02x:%02x:%02x:%02x:%02x:%02x]\n", i, 
+        printf("\t\t\t[%d] [MAC: %02x:%02x:%02x:%02x:%02x:%02x]\n", i,
                entry.mac[i][0], entry.mac[i][1], entry.mac[i][2],
                entry.mac[i][3], entry.mac[i][4], entry.mac[i][5]);
-
       }
     }
     // Update key to next_key for the next iteration
@@ -78,6 +84,13 @@ int main() {
   // Set program type to XDP
   bpf_program__set_type(skel->progs.xdp_dad, BPF_PROG_TYPE_XDP);
 
+  // Get the interface index of the desired interface (e.g., ens3)
+  int ifindex = if_nametoindex("ens3");
+  if (ifindex == 0) {
+    perror("if_nametoindex");
+    goto cleanup;
+  }
+
   // load xdp
   err = xdp_dad_bpf__load(skel);
   if (err) {
@@ -85,8 +98,12 @@ int main() {
     goto cleanup;
   }
 
+  xdp_flags = 0;
+  xdp_flags |= XDP_FLAGS_DRV_MODE;
+  xdp_flags |= XDP_FLAGS_UPDATE_IF_NOEXIST;
+
   // attach prog
-  err = xdp_dad_bpf__attach(skel);
+  err = bpf_xdp_attach(ifindex, bpf_program__fd(skel->progs.xdp_dad), xdp_flags, NULL);
   if (err) {
     printf("Error while attaching BPF skeleton");
     goto cleanup;
@@ -95,18 +112,37 @@ int main() {
   printf("Successfully attached!\n\n");
 
   // loop until char c is received
+  char input;
+  int exit_flag = 0;
+  struct pollfd mypoll = { STDIN_FILENO, POLLIN|POLLPRI };
 
-  printf("Type 'c' to exit...\n");
-  while (getchar() != 'c') {
+  printf("Type 'q' to exit...\n");
+  while (!exit_flag) {
+    
+    // Check if there's input available
+    if( poll(&mypoll, 1, 2000) )
+    {
+      scanf("%c", &input);
+      if (input == 'q') {
+        exit_flag = 1; // Set flag to exit the loop
+      }
+    }
 
     if (print_map_data(skel) < 0) {
       printf("Error while printing map data");
-      goto cleanup;
+      goto detach;
     }
 
-    sleep(1);
+    //sleep(1);
   }
   printf("Bye\n");
+
+detach:
+  // detach prog
+  err = bpf_xdp_detach(ifindex, xdp_flags, NULL);
+  if (err) {
+    printf("Error while detaching XDP from interface");
+  }
 
 cleanup:
   xdp_dad_bpf__destroy(skel);
